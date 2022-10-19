9a297a1b81daf780beace4c53fadf3eb6811bc81
ProTiana <110047208+ProTiana@users.noreply.github.com>
2022-10-19 22:39:50 +0300

    Update compl.py

---
+++ compl.py
@@ -0,0 +1,14 @@
+def calc(num1, num2, znak):
+    if znak == '+':
+        return num1 + num2
+
+    elif znak == '-':
+        return num1 - num2
+
+    elif znak == '*':
+        return num1 * num2
+
+    elif znak == '/':
+        return num1 / num2
+
+print(calc(num1, num2, znak))
