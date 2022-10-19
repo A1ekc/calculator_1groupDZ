f3475993c24b9f3b8b2fcbbe33e8f4fd9025e77b
Alex <a1ekcbend@gmail.com>
2022-10-19 15:20:42 +0300

    откорректирован

--- controller.py
+++ controller.py
@@ -1,20 +1,18 @@
 #файл объединяет отдельные методы
-
+import view 
 import compl
 import rational
-import view
 import logger
 
 
 def button_click():# Опишим метод кнопки
     value_a = view.get_value()
     value_b = view.get_value()
-    compl.init(value_a, value_b)
+    value_oper = view.get_value()
+    compl.init(value_a, value_b, value_oper)# ?
+    rational.init(value_a, value_b,value_oper)# ?
     #result = ex046_lec.sum()
-    result = compl()
-    result = rational()
-    view.view_data(result)
-
-    # Кудато тут нужно вписать лог))
-    write_log
+    #result = compl()
+    #result = rational()
+    #view.view_data(result)
 
